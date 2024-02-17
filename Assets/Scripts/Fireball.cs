260ea1e12ad106c7d567192130e0352c02d361c5
azentaa <158740961+azentaa@users.noreply.github.com>
2024-02-17 19:26:25 +0700

    5

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public int speed;
+    public float lifeTime;
+
+    private void Start()
+    {
+        Invoke("DestroyFireball", lifeTime);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        DestroyFireball();
+    }
+
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/FireballSource.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballSource : MonoBehaviour
+{
+    public Transform targetPoint;
+   
+    void Update()
+    {
+        transform.LookAt(targetPoint.position);    
+    }
+}
